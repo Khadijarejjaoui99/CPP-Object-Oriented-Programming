a9861f9d355791fad9197e36494135c7b7578718
KRCodeNinja <khadijarejjaoui41@gmail.com>
2024-03-05 21:28:06 +0100

    Example 1 in main.cpp

--- Project-2-String-Library/main.cpp
+++ Project-2-String-Library/main.cpp
@@ -4,7 +4,17 @@ using namespace std;
 
 int main()
 {
+     // Example 1:
 
+     clsString String1("Hello world!");
+     clsString String2;
+
+     String2.SetValue("Programming is Fun");
+
+     cout << "\n String1: " << String1.GetValue();
+     cout << "\n String2: " << String2.GetValue() << endl;
+
+     //
      system("pause>0");
      return 0;
 }
