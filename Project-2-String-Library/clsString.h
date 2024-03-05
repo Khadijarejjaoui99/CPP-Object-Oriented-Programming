f7160f12e66473e52a36cf068ab0abac89b32406
KRCodeNinja <khadijarejjaoui41@gmail.com>
2024-03-05 13:01:37 +0100

    Trim methods

--- Project-2-String-Library/clsString.h
+++ Project-2-String-Library/clsString.h
@@ -284,4 +284,51 @@ public:
     {
         return SplitString(_Value, Delim);
     }
+
+    static string TrimLeft(string S1)
+    {
+        for (short i = 0; i < S1.length(); i++)
+        {
+            if (S1[i] != ' ')
+            {
+                return S1.substr(i, S1.length() - i);
+            }
+        }
+
+        return "";
+    }
+
+    string TrimLeft()
+    {
+        return TrimLeft(_Value);
+    }
+
+    static string TrimRight(string S1)
+    {
+
+        for (short i = S1.length() - 1; i >= 0; i--)
+        {
+            if (S1[i] != ' ')
+            {
+                return S1.substr(0, i + 1);
+            }
+        }
+
+        return "";
+    }
+
+    string TrimRight()
+    {
+        return TrimRight(_Value);
+    }
+
+    static string Trim(string S1)
+    {
+        return TrimLeft(TrimRight(S1));
+    }
+
+    string Trim()
+    {
+        return Trim(_Value);
+    }
 };
