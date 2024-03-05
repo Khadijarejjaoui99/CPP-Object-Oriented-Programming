e7d08424d8b26535d4f697b046b41f18a1b04d71
KRCodeNinja <khadijarejjaoui41@gmail.com>
2024-03-05 21:35:18 +0100

    Example 2 in main.cpp

--- Project-2-String-Library/main.cpp
+++ Project-2-String-Library/main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main()
 {
      // Example 1:
+     cout << "\n Example 1:\n";
 
      clsString String1("Hello world!");
      clsString String2;
@@ -14,7 +15,13 @@ int main()
      cout << "\n String1: " << String1.GetValue();
      cout << "\n String2: " << String2.GetValue() << endl;
 
-     //
+     // Example 2:
+     cout << "\n Example 2:\n";
+
+     cout << "\n Capital Letters Count Of String1: " << String1.CountCapitalLetters();
+     cout << "\n Capital Letters Count Of String2: " << String2.CountCapitalLetters();
+     cout << "\n Capital Letters Count Of \'Hi, How are yOU Today\': " << clsString::CountCapitalLetters("Hi, How are yOU Today") << endl;
+
      system("pause>0");
      return 0;
 }
