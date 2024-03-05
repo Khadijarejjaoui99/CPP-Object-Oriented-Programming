d0e589400315575966f65864cbf7d40498b440f9
KRCodeNinja <khadijarejjaoui41@gmail.com>
2024-03-05 21:53:08 +0100

    Example 6 CountVowels() method

--- Project-2-String-Library/main.cpp
+++ Project-2-String-Library/main.cpp
@@ -18,23 +18,23 @@ int main()
      // Example 2:
      cout << "\n Example 2:\n";
 
-     cout << "\n Capital Letters Count Of String1: " << String1.CountCapitalLetters();
-     cout << "\n Capital Letters Count Of String2: " << String2.CountCapitalLetters();
-     cout << "\n Capital Letters Count Of \'Hi, How are yOU Today\': " << clsString::CountCapitalLetters("Hi, How are yOU Today") << endl;
+     cout << "\n Capital Letters Count in String1: " << String1.CountCapitalLetters();
+     cout << "\n Capital Letters Count in String2: " << String2.CountCapitalLetters();
+     cout << "\n Capital Letters Count in \'Hi, How are yOU Today\': " << clsString::CountCapitalLetters("Hi, How are yOU Today") << endl;
 
      // Example 3:
      cout << "\n Example 3:\n";
 
-     cout << "\n Capital Letters Count Of \'Hi, How are yOU Today\': " << clsString::CountLetters("Hi, How are yOU Today", clsString::CapitalLetters);
-     cout << "\n Small Letters Count Of \'Hi, How are yOU Today\': " << clsString::CountLetters("Hi, How are yOU Today", clsString::SmallLetters);
-     cout << "\n All Letters Count Of \'Hi, How are yOU Today\': " << clsString::CountLetters("Hi, How are yOU Today", clsString::All) << endl;
+     cout << "\n Capital Letters Count in \'Hi, How are yOU Today\': " << clsString::CountLetters("Hi, How are yOU Today", clsString::CapitalLetters);
+     cout << "\n Small Letters Count in \'Hi, How are yOU Today\': " << clsString::CountLetters("Hi, How are yOU Today", clsString::SmallLetters);
+     cout << "\n All Letters Count in \'Hi, How are yOU Today\': " << clsString::CountLetters("Hi, How are yOU Today", clsString::All) << endl;
 
      // Example 4:
      cout << "\n Example 4:\n";
 
-     cout << "\n Small Letters Count Of String1: " << String1.CountSmallLetters();
-     cout << "\n Small Letters Count Of String2: " << String2.CountSmallLetters();
-     cout << "\n Small Letters Count Of \'Hi, How are yOU Today\': " << clsString::CountSmallLetters("Hi, How are yOU Today") << endl;
+     cout << "\n Small Letters Count in String1: " << String1.CountSmallLetters();
+     cout << "\n Small Letters Count in String2: " << String2.CountSmallLetters();
+     cout << "\n Small Letters Count in \'Hi, How are yOU Today\': " << clsString::CountSmallLetters("Hi, How are yOU Today") << endl;
 
      // Example 5:
      cout << "\n Example 5:\n";
@@ -43,6 +43,13 @@ int main()
      cout << "\n letter l count in String1 (Without Matching case): " << String1.CountSpecificLetter('l', false);
      cout << "\n letter o count in \'Hi, How are yOU Today\' (Matching case): " << clsString::CountSpecificLetter("Hi, How are yOU Today", 'o');
      cout << "\n letter o count in \'Hi, How are yOU Today\' (Without Matching case): " << clsString::CountSpecificLetter("Hi, How are yOU Today", 'o', false) << endl;
+
+     // Example 6:
+     cout << "\n Example 6:\n";
+
+     cout << "\n Vowels Count in String1: " << String1.CountVowels();
+     cout << "\n Vowels count in String1: " << String2.CountVowels();
+     cout << "\n Vowels count in \'Hi, How are yOU Today\': " << clsString::CountVowels("Hi, How are yOU Today") << endl;
 
      system("pause>0");
      return 0;
